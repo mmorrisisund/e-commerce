73997dfc67585f5f79e3c116b4e13ca61fe28858
Mike <michael.a.morris.public@gmail.com>
2021-05-08 18:19:31 -0500

    initial commit

---
+++ server/validation/addPetSchema.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+
+module.exports = Joi.object({
+  name: Joi.string().required(),
+  type: Joi.any().valid('dog', 'cat').required(),
+  dob: Joi.date().timestamp().required(),
+  description: Joi.string().required(),
+  sex: Joi.any().valid('male', 'female').required(),
+  spayed: Joi.boolean().optional(),
+  breed: Joi.string().required(),
+})
