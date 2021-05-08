9c7e0b156d7d2b1f2ed40c5bc47be884a2685971
Mike <michael.a.morris.public@gmail.com>
2021-05-08 17:25:56 -0500

    add primary auth routes and controllers

--- server/api/auth/auth.controllers.js
+++ server/api/auth/auth.controllers.js
@@ -1,3 +1,55 @@
-exports.test = (req, res) => {
-  res.json({ status: 'success', data: { user: 'user data' } })
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/user')
+const Password = require('../../models/password')
+const catchAsync = require('../../utils/catchAsync')
+const generateToken = require('../../utils/generateToken')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+exports.signUp = catchAsync(async (req, res) => {
+  const { email, password } = req.body
+
+  const pw = new Password({ password })
+  const user = new User({ email, password: pw._id })
+
+  try {
+    await pw.save()
+    await user.save()
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.json({
+        status: 'fail',
+        data: { email: 'Email already exists' },
+      })
+    }
+  }
+
+  res.json({ status: 'success', data: { user } })
+})
+
+exports.login = catchAsync(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email }).populate('password').exec()
+  if (!user) {
+    return res.json({
+      status: 'fail',
+      data: { email: 'Email address not found' },
+    })
+  }
+
+  const isValid = await bcrypt.compare(password, user.password.password)
+  if (!isValid) {
+    return res.json({ status: 'fail', data: { password: 'Invalid password' } })
+  }
+
+  token = generateToken(user._id)
+  res.cookie('token', token, { httpOnly: true, sameSite: true, secure: !isDev })
+  res.json({ status: 'success', data: null })
+})
+
+exports.logout = (req, res) => {
+  res.clearCookie('token')
+  res.json({ status: 'success', data: null })
 }
--- server/api/auth/auth.routes.js
+++ server/api/auth/auth.routes.js
@@ -1,9 +1,14 @@
-const { test } = require('./auth.controllers')
+const validate = require('../../middleware/validate')
+const signupSchema = require('../../validation/signupSchema')
+const loginSchema = require('../../validation/loginSchema')
+const { signUp, login, logout } = require('./auth.controllers')
 
 module.exports = Router => {
   const router = new Router()
 
-  router.get('/', test)
+  router.post('/signup', validate(signupSchema), signUp)
+  router.post('/login', validate(loginSchema), login)
+  router.post('/logout', logout)
 
   return { prefix: '/auth', router }
 }
