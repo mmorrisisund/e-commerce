f5bb2ff91a7828fcf218797d4b2330d7d92847aa
Mike <michael.a.morris.public@gmail.com>
2021-05-08 18:30:05 -0500

    add create route

--- server/api/pets/pets.routes.js
+++ server/api/pets/pets.routes.js
@@ -1,5 +1,11 @@
+const validate = require('../../middleware/validate')
+const addPetSchema = require('../../validation/addPetSchema')
+const { addPet } = require('./pets.controllers')
+
 module.exports = Router => {
   router = new Router()
+
+  router.post('/add', validate(addPetSchema), addPet)
 
   return { prefix: '/pets', router }
 }
