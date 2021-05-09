5684e9884b585e826472e55d14d6b8771fa4fa10
Mike <michael.a.morris.public@gmail.com>
2021-05-08 23:46:42 -0500

    add remaing crud routes for pets

--- server/api/pets/pets.controllers.js
+++ server/api/pets/pets.controllers.js
@@ -7,3 +7,37 @@ exports.addPet = catchAsync(async (req, res) => {
 
   res.json({ status: 'success', data: { pet } })
 })
+
+exports.getPet = catchAsync(async (req, res) => {
+  const { id } = req.body
+  const pet = await Pet.findById(id)
+
+  if (pet) {
+    res.json({ status: 'success', data: { pet } })
+  } else {
+    res.json({ status: 'fail', data: { message: 'Pet not found' } })
+  }
+})
+
+exports.updatePet = catchAsync(async (req, res) => {
+  const { id, ...rest } = req.body
+  const pet = await Pet.findByIdAndUpdate(id, { ...rest })
+
+  if (pet) {
+    const updatedPet = { ...pet._doc, ...rest }
+    res.json({ status: 'success', data: { pet: updatedPet } })
+  } else {
+    res.json({ status: 'fail', data: { message: 'Error updating pet.' } })
+  }
+})
+
+exports.deletePet = catchAsync(async (req, res) => {
+  const { id } = req.body
+  const pet = await Pet.findByIdAndDelete(id)
+
+  if (pet) {
+    res.json({ status: 'success', data: null })
+  } else {
+    res.json({ status: 'fail', data: { message: 'Error deleting pet' } })
+  }
+})
--- server/api/pets/pets.routes.js
+++ server/api/pets/pets.routes.js
@@ -1,11 +1,15 @@
 const validate = require('../../middleware/validate')
 const addPetSchema = require('../../validation/addPetSchema')
-const { addPet } = require('./pets.controllers')
+const updatePetSchema = require('../../validation/updatePetSchema')
+const { addPet, getPet, updatePet, deletePet } = require('./pets.controllers')
 
 module.exports = Router => {
   router = new Router()
 
   router.post('/add', validate(addPetSchema), addPet)
+  router.post('/single', getPet)
+  router.post('/update', validate(updatePetSchema), updatePet)
+  router.post('/delete', deletePet)
 
   return { prefix: '/pets', router }
 }
