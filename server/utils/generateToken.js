220e348608770c7595e5293834719ab62afe57bd
Mike <michael.a.morris.public@gmail.com>
2021-05-09 18:02:37 -0500

    remove stringify on payload

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = payload => {
-  return jwt.sign(JSON.stringify(payload), process.env.JWT_SECRET)
+  return jwt.sign(payload, process.env.JWT_SECRET)
 }
