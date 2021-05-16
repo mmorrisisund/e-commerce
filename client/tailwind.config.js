2344b51989fbede883a641dbb7a6016333fb2d8f
Mike <michael.a.morris.public@gmail.com>
2021-05-15 21:44:29 -0500

    add custom properties

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      '2xs': '320px',
+      xs: '480px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      backgroundImage: {
+        hero: 'url("./assets/images/banner.png")',
+      },
+      fontFamily: {
+        pacifico: 'Pacifico',
+      },
+    },
   },
   variants: {
     extend: {},
