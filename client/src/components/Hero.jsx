e0aa1b1510b75cdf0245e96f6b416b267422074e
Mike <michael.a.morris.public@gmail.com>
2021-05-16 17:57:44 -0500

    hide logo on larger screens

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import banner from '../assets/images/banner.png'
-import HeroLogo from './HeroLogo'
+import Logo from './Logo'
 import Button from './shared/Button'
 
 const Hero = () => {
@@ -8,13 +8,16 @@ const Hero = () => {
     <div className='relative'>
       <img className='w-full' src={banner} alt='banner' />
       <div className='absolute inset-0 flex items-center justify-center'>
-        <div className='p-4 xs:p-6 lg:p-8 xl:p-14 flex flex-col items-center justify-center bg-white bg-opacity-50  rounded-2xl'>
+        <div className='p-4 w-2/3 h-1/2 xs:p-6 lg:p-8 xl:p-14 flex flex-col items-center justify-center bg-white bg-opacity-50  rounded-2xl'>
           <div className='flex flex-col items-center'>
-            <HeroLogo />
-            <span className='text-green-800 text-lg xs:text-xl lg:text-3xl xl:text-4xl lg:tracking-wide'>
+            <div className='sm:hidden'>
+              <Logo />
+            </div>
+            <span className='text-green-800 text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl lg:tracking-wide'>
               Find your next best friend!
             </span>
           </div>
+
           <div className='flex space-x-2 xs:space-x-4 justify-center w-full xs:mt-4 lg:mt-8'>
             <Link to='/search'>
               <Button variant='secondary' size='md'>
