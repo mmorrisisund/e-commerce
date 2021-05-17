175cac3e281b28f36a0e44e6904243dc89eb2322
Mike <michael.a.morris.public@gmail.com>
2021-05-16 19:37:26 -0500

    remove development message

--- server/middleware/validate.js
+++ server/middleware/validate.js
@@ -6,12 +6,7 @@ module.exports = schema => (req, res, next) => {
   if (error) {
     const { details } = error
     const message = details.map(i => i.message).join('\n')
-
-    if (process.env.NODE_ENV === 'development') {
-      return res.json({ status: 'fail', data: { error } })
-    } else {
-      return res.json({ status: 'fail', data: { message } })
-    }
+    return res.json({ status: 'fail', data: { message } })
   }
   next()
 }
