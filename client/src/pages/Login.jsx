97db7b36158a1e8cce1cfa1d211d39fc6fbfe53a
Mike <michael.a.morris.public@gmail.com>
2021-05-16 20:21:55 -0500

    add redirect after login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,20 @@
-import LoginForm from '../components/LoginForm'
+import { Redirect } from 'react-router-dom'
 
-const Login = () => (
-  <main className='p-1 pt-8 sm:p-8'>
-    <LoginForm />
-  </main>
-)
+import LoginForm from '../components/LoginForm'
+import { useAuth } from '../context/authContext'
+
+const Login = () => {
+  const { isAuthenticated } = useAuth()
+
+  if (isAuthenticated) {
+    return <Redirect to='/' />
+  }
+
+  return (
+    <main className='p-1 pt-8 sm:p-8'>
+      <LoginForm />
+    </main>
+  )
+}
 
 export default Login
