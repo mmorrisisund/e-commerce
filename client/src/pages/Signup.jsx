b266d3ebccc8c5cee287f0369c9645e2b95421c5
Mike <michael.a.morris.public@gmail.com>
2021-05-22 11:56:25 -0500

    return to main page if logged in

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,17 @@
+import { Redirect } from 'react-router-dom'
+
+import { useAuth } from '../context/authContext'
 import Glass from '../components/shared/Glass'
 import ImageContainer from '../components/shared/ImageContainer'
 import SignupForm from '../components/SignupForm'
 
 const Signup = () => {
+  const { isAuthenticated } = useAuth()
+
+  if (isAuthenticated) {
+    return <Redirect to='/' />
+  }
+
   return (
     <main>
       <ImageContainer src='https://res.cloudinary.com/mimo/image/upload/v1621560133/pawsitive-pals/assets/signup-bg.jpg'>
