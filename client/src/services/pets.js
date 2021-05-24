e61507aab433f9622564c5b61318c82656e54709
Mike <michael.a.morris.public@gmail.com>
2021-05-23 23:26:29 -0500

    add util to search pets

--- client/src/services/pets.js
+++ client/src/services/pets.js
@@ -1,3 +1,4 @@
+import { getYearsAgo } from '../utils/dates'
 import http from '../utils/http-common'
 
 export const getPetsWithinRadius = async (center, distance) => {
@@ -5,6 +6,29 @@ export const getPetsWithinRadius = async (center, distance) => {
     limit: 5,
     within: { center, distance },
     fields: 'image,name',
+  })
+
+  return data.data.pets
+}
+
+export const getPets = async ({
+  type = 'dog',
+  minAge = 0,
+  maxAge = 15,
+  gender = 'any',
+  center = [-103.771556, 44.967243],
+  distance = 500,
+}) => {
+  const { data } = await http.post('/pets/all', {
+    limit: 10,
+    fields: 'image, name',
+    type,
+    dob: {
+      lt: getYearsAgo(Date.now(), minAge),
+      gt: getYearsAgo(Date.now(), maxAge),
+    },
+    sex: gender.toLowerCase() === 'any' ? undefined : gender,
+    within: { center, distance },
   })
 
   return data.data.pets
